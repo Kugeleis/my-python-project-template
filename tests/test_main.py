26983c6741dec566125a48857d60f94b01493940
Kugeleis <9948777+Kugeleis@users.noreply.github.com>
2026-05-08 18:58:18 +0900

    stricter code checks

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,9 @@
+import pytest
+
 from my_python_project_template import main
 
 
-def test_main(capsys):
+def test_main(capsys: pytest.CaptureFixture[str]) -> None:
     main()
     captured = capsys.readouterr()
     assert captured.out == "Hello from my-python-project-template!\n"
